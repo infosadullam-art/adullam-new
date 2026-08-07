079a777430aa880a19bba7a0a0584510b5f92769
infosadullam-art <infos.adullam@gmail.com>
2026-08-07 17:34:41 +0000

    fix: remove right overflow on desktop for DealCountdown

--- components/deal-countdown.tsx
+++ components/deal-countdown.tsx
@@ -230,7 +230,7 @@ export function DealCountdown() {
   }
 
   return (
-    <div className="w-full" style={{ background: "#FAFAFA" }}>
+    <div className="w-full lg:-mx-0" style={{ background: "#FAFAFA" }}>
       <div className="relative overflow-hidden" style={{ background: "#fff" }}>
         <div
           className="pointer-events-none absolute inset-0 opacity-[0.04] animate-pulse-slow"
